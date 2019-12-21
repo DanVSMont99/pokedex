4705536338a18d9a296a84a816f3f1089e45ff21
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-21 15:44:54 -0300

    Initial structure

---
+++ src/routes/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+import AddPokemon from '../pages/AddPokemon'
+import UpdatePokemon from '../pages/UpdatePokemon'
+
+export default () => 
+  <BrowserRouter>
+    <Switch name='http://localhost:3000'>
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} />
+      <Route path="/home" component={Home} />
+      <Route path="/addpokemon" component={AddPokemon} />
+      <Route path="/updatepokemon" component={UpdatePokemon} />
+      <Route component={Home} />
+    </Switch>
+  </BrowserRouter>
