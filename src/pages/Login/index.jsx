70480fcbf8926de200d840fe8627d65e8c0a12f0
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-23 15:01:29 -0300

    Add login age

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+import pokeball from '../../assets/pokeball.png'
+import './styles.scss'
 
-export default () => <h1>Login</h1>
+export default () => 
+  <div className='login'> 
+    <main>
+      <header>
+        <h1>Pokedex</h1>
+        <h2>Seja bem vindo treinador</h2>
+        <img src={pokeball} alt='Pokeball'/>
+      </header>
+      <form>
+        <Input type='email' label='Email' width='100%' placeholder='Digite seu email' />
+        <Input type='password' label='Senha' width='100%' placeholder='Digite sua senha' />
+        <Link to='/home'>
+          <Button label='Entrar agora' width='100%' />
+        </Link>
+      </form>
+    </main>
+  </div>
