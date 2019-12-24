f7f69709a05884a69e9fbb4922168b9e552ced7a
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-24 16:40:23 -0300

    Update icons

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -7,8 +7,8 @@ export default props => {
   }
 
   const iconSize = {
-    width: props.width,
-    height: props.height
+    width: props.size,
+    height: props.size
   }
 
   return (
@@ -19,20 +19,38 @@ export default props => {
   )
 }
 
-export const AddIcon = props => {
+export const Add = props => {
   const iconColor = { 
     fill: props.color
   }
 
   const iconSize = {
-    width: props.width,
-    height: props.height
+    width: props.size,
+    height: props.size
   }
 
   return (
     <svg  style={iconSize} width="24" height="24" viewBox="0 0 24 24">
       <path style={iconColor} d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
       <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
+  )
+}
+
+export const Image = props => {
+  const iconColor = { 
+    fill: props.color
+  }
+
+  const iconSize = {
+    width: props.size,
+    height: props.size
+  }
+
+  return (
+    <svg style={iconSize} width="24" height="24" viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path style={iconColor} d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/>
     </svg>
   )
 }
