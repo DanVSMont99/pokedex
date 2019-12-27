f19703ed82d06d9badd4ba560aecc00a2f786acc
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-27 16:05:13 -0300

    Add fixed button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Add } from '../Icon'
 import './styles.scss'
 
 export default props => {
@@ -19,3 +20,19 @@ export default props => {
     </button>
   )
 }
+
+export const FixedButton = props => {
+  const buttonStyle = {
+    top: props.top,
+    left: props.left,
+    right: props.right,
+    bottom: props.bottom,
+    backgroundColor: props.backgroundColor,
+  }
+
+  return (
+    <button className='button fixed' style={buttonStyle}>
+      <Add size={props.size || '6rem'} color={props.color || 'white'} />
+    </button>
+  )
+} 
