60b7416c4753edcd9b54b24182e6188ffa53ae19
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-12-27 22:02:37 -0300

    Add home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { loadPokemons } from '../../store/actions/pokemons'
+import api from '../../services/api'
 import Header from '../../components/Header'
+import Card from '../../components/Card'
+import { FixedButton } from '../../components/Button'
 import './styles.scss'
 
-export default () =>  
-  <>
-    <Header />
-    <main className='home'>
-      <h1>Home</h1>
-    </main>
-  </>
+function Home(props) {
+  async function loadPokemons() {
+    const response = await api.get('pokemons')
+    const pokemons = response.data
+    props.loadPokemons(pokemons)
+  }
+
+  function renderPokemons(pokemons) {
+    const pokemonsCards = pokemons.map((pokemon, i) => 
+      <Card key={i} text={pokemon.name} code={pokemon.number} />
+    )
+      
+    return pokemonsCards
+  }
+
+  loadPokemons()
+
+  return (
+    <div className='home'>
+      <Header />
+      <Link to='/addpokemon'>
+        <FixedButton />
+      </Link>
+      <main>
+        {renderPokemons(props.pokemons)}
+      </main>
+    </div>
+  )
+}
+  
+const mapStateToProps = state => ({ pokemons: state.pokemons })
+const mapDispatchToProps = dispatch => bindActionCreators({ loadPokemons }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
