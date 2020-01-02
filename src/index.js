d5428c6ae1deca4b643998bdad8bde6af98a9328
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-01-02 15:44:56 -0300

    Update styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
 import './styles/reset.scss'
-import './styles/custom.scss'
+import './styles/global.scss'
 
 ReactDOM.render(
   <Routes />,
