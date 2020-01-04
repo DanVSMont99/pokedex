289663143f769cd7b9b1101e1311c6881ef6060c
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2020-01-04 16:25:13 -0300

    Add redux persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './store'
+import { store, persistor } from './store'
 import Routes from './routes'
 
 import './styles/reset.scss'
@@ -10,7 +11,9 @@ import './styles/global.scss'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Routes />
+    <PersistGate loading={null} persistor={persistor}>
+      <Routes />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { createStore, combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import PokemonReducer from './reducers/pokemonReducer'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
 
 const reducers = combineReducers({
   pokemons: PokemonReducer
 })
 
-const store = createStore(reducers)
+const persistedReducer = persistReducer(persistConfig, reducers)
 
-export default store
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
+
+export { store, persistor }
